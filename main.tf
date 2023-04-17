57108ded32a349040157ff141fadf74a9cf29019
Pawan-666 <eulogy1996@gmail.com>
2023-04-17 14:46:04 +0545

    Added variables

--- main.tf
+++ main.tf
@@ -1,15 +1,8 @@
-locals {
-  public_cidr = ["10.0.0.0/24", "10.0.1.0/24"] 
-  private_cidr = ["10.0.2.0/24", "10.0.3.0/24"]
-  availability_zones = ["ap-southeast-1a", "ap-southeast-1b"]
-}
-
-
 resource "aws_vpc" "main" {
-  cidr_block = "10.0.0.0/16"
+  cidr_block = var.vpc_cidr
 
   tags = {
-    Name = "main"
+    Name = "${var.env_code}"
   }
 }
 
@@ -17,11 +10,11 @@ resource "aws_subnet" "public" {
   count = 2
 
   vpc_id     = aws_vpc.main.id
-  cidr_block = local.public_cidr[count.index]
-  availability_zone = local.availability_zones[count.index]
+  cidr_block = var.public_cidr[count.index]
+  availability_zone = var.availability_zones[count.index]
 
   tags = {
-    Name = "public${count.index + 1}"
+    Name = "${var.env_code}-public${count.index + 1}"
   }
 }
 
@@ -29,11 +22,11 @@ resource "aws_subnet" "public" {
 resource "aws_subnet" "private" {
   count = 2
   vpc_id     = aws_vpc.main.id
-  cidr_block = local.private_cidr[count.index]
-  availability_zone = local.availability_zones[count.index]
+  cidr_block = var.private_cidr[count.index]
+  availability_zone = var.availability_zones[count.index]
 
   tags = {
-    Name = "private${count.index + 1}"
+    Name = "${var.env_code}-private${count.index + 1}"
   }
 }
 
@@ -42,7 +35,7 @@ resource "aws_internet_gateway" "main" {
   vpc_id = aws_vpc.main.id
 
   tags = {
-    Name = "main"
+    Name = var.env_code
   }
 }
 
@@ -66,6 +59,10 @@ resource "aws_route_table_association" "public" {
 resource "aws_eip" "nat" {
   count = 2
   vpc      = true
+
+  tags = {
+    Name = "${var.env_code}${count.index+1}"
+  }
 }
 
 
@@ -73,6 +70,10 @@ resource "aws_nat_gateway" "nat" {
   count = 2
   allocation_id = aws_eip.nat[count.index].id
   subnet_id     = aws_subnet.public[count.index].id
+
+  tags = {
+    Name = "${var.env_code}${count.index+1}"
+  }
 }
 
 
@@ -83,6 +84,9 @@ resource "aws_route_table" "private" {
   route {
       cidr_block = "0.0.0.0/0"
       nat_gateway_id = aws_nat_gateway.nat[count.index].id
+  }
+  tags = {
+    Name = "${var.env_code}-private${count.index+1}"
   }
 }
 
